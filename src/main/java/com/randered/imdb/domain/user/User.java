e7df9cdac25897f4cb181dab63d65ec821af13a9
randered <68559794+randered@users.noreply.github.com>
2022-09-20 11:09:15 +0300

    added user entity and base entity

---
+++ src/main/java/com/randered/imdb/domain/user/User.java
@@ -0,0 +1,25 @@
+package com.randered.imdb.domain.user;
+
+import com.randered.imdb.domain.base.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+}
